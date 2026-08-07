ae94c571316c6dc9d23c7d2ae5456d83f92ada5a
proyectomusical010101-create <proyectomusical010101@gmail.com>
2026-08-07 13:55:30 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,35 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-class AppErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.error("Recovering App:", error);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      try {
-        localStorage.clear();
-      } catch (e) {}
-      return <App />;
-    }
-    return this.props.children;
-  }
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppErrorBoundary>
-      <App />
-    </AppErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
